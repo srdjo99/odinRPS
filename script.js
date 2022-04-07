851298e90c72978bf8365c835e83c115dde8cd65
Srdjan <srdjancoralic5@gmail.com>
2022-04-07 16:57:51 +0200

    Game logic finished

--- script.js
+++ script.js
@@ -1,38 +1,72 @@
 // My own way of Rock, Paper, Scissors
 
+const buttons = document.querySelectorAll("button");
+const results = document.querySelector(".results");
+const para = document.createElement("p");
+const winner = document.createElement("h3");
+const compScore = document.querySelector(".computer-score");
+const playerScore = document.querySelector(".player-score");
+
+let computerPoints = 0;
+let playerPoints = 0;
+let computerChoice;
+let playerChoice;
+
+// Getting computer random choice
 function computerPlay() {
   const computerArr = ["rock", "paper", "scissors"];
   return computerArr[Math.floor(Math.random() * computerArr.length)];
 }
 
+// Game logic
 function playRound(playerSelection, computerSelection) {
-  // your code here!
-
-  const playerChoice = playerSelection.toLowerCase();
-  // console.log(playerChoice);
   if (
     (computerSelection === "rock" && playerChoice === "scissors") ||
     (computerSelection === "paper" && playerChoice === "rock") ||
     (computerSelection === "scissors" && playerChoice === "paper")
   ) {
-    console.log(`Computer won: ${computerSelection} beats ${playerChoice}`);
+    para.textContent = `Computer won: ${computerSelection} beats ${playerChoice}`;
+    results.append(para);
+
+    computerPoints++;
+    compScore.innerHTML = `Computer points: ${computerPoints}`;
+
+    if (computerPoints === 5) {
+      winner.innerHTML = "Computer WON";
+      results.append(winner);
+    }
   } else if (
     (playerChoice === "rock" && computerSelection === "scissors") ||
     (playerChoice === "scissors" && computerSelection === "paper") ||
     (playerChoice === "paper" && computerSelection === "rock")
   ) {
-    console.log(`Player won: ${playerChoice} beats ${computerSelection}`);
+    para.textContent = `Player won: ${playerChoice} beats ${computerSelection}`;
+    results.append(para);
+
+    playerPoints++;
+    playerScore.innerHTML = `Player points: ${playerPoints}`;
+
+    if (playerPoints === 5) {
+      winner.innerHTML = "You WON";
+      results.append(winner);
+    }
   } else {
-    console.log(`Tie: ${playerChoice} does NOT beat ${computerSelection}`);
+    para.textContent = `Tie: ${playerChoice} does NOT beat ${computerSelection}`;
+    results.append(para);
   }
 }
 
-for (let i = 0; i < 5; i++) {
-  // your code here!
-  const playerSelection = prompt(
-    "Enter: rock, paper or scissors!"
-  ).toLowerCase();
-
-  const computerSelection = computerPlay();
-  playRound(playerSelection, computerSelection);
+// Callback function when button is clicked
+function playing(e) {
+  if (computerPoints === 5 || playerPoints === 5) {
+    alert("Game finished, it was race to 5 😎");
+    return;
+  } else {
+    playerChoice = e.target.className;
+    computerChoice = computerPlay();
+    playRound(playerChoice, computerChoice);
+  }
 }
+
+// Event handler
+buttons.forEach((button) => button.addEventListener("click", playing));
